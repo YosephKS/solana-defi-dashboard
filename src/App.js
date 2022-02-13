03f554aecbe6980c43f4a046aae8cc6cbd4145f3
Yoseph Kurnia Soenggoro <yosephks@Yosephs-MacBook-Pro.local>
2022-02-13 21:19:05 +0400

    setting up the authentication and token display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,139 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import {
+  useMoralis,
+  useMoralisSolanaApi,
+  useMoralisSolanaCall,
+} from "react-moralis";
+import {
+  Blockie,
+  Button,
+  Select,
+  Loading,
+  Table,
+  Avatar,
+  Tag,
+  getEllipsisTxt,
+} from "web3uikit";
 
 function App() {
+  const [network, setNetwork] = useState("mainnet");
+  const {
+    isAuthenticated,
+    authenticate,
+    user,
+    isInitializing,
+    isInitialized,
+    isAuthenticating,
+    logout,
+  } = useMoralis();
+  const { account } = useMoralisSolanaApi();
+  const { fetch, data, isLoading } = useMoralisSolanaCall(account.getPortfolio);
+
+  useEffect(() => {
+    if (isAuthenticated && user.get("solAddress")) {
+      fetch({
+        params: {
+          address: user.get("solAddress"),
+          network,
+        },
+      });
+    }
+  }, [fetch, isAuthenticated, user, network]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "1rem",
+      }}
+    >
+      {!isInitialized || isInitializing ? (
+        <Loading spinnerColor="#2E7DAF" size={50} />
+      ) : (
+        <>
+          {isAuthenticated ? (
+            <>
+              <Blockie seed={user?.get("solAddress")} size={20} />
+              <p>{user?.get("solAddress")}</p>
+              <Button onClick={() => logout()} text="Disconnect Wallet" />
+              <Select
+                label="Network"
+                onChange={(option) => {
+                  const { id } = option ?? {};
+                  setNetwork(id);
+                }}
+                defaultOptionIndex={0}
+                options={[
+                  {
+                    id: "mainnet",
+                    label: "Mainnet",
+                  },
+                  {
+                    id: "devnet",
+                    label: "Devnet",
+                  },
+                ]}
+              />
+              {isLoading ? (
+                <Loading spinnerColor="#2E7DAF" text="Fetching Data..." />
+              ) : (
+                <Table
+                  columnsConfig="80px 3fr 2fr 2fr"
+                  data={[
+                    [
+                      <Avatar isRounded theme="image" />,
+                      "SOL",
+                      <Tag color="blue" text="Native Token" />,
+                      data?.nativeBalance?.solana,
+                    ],
+                    ...data?.tokens?.map((token) => {
+                      return [
+                        <Avatar isRounded theme="image" />,
+                        getEllipsisTxt(token.associatedTokenAddress),
+                        <Tag color="red" text="SPL Token" />,
+                        token.amount,
+                      ];
+                    }),
+                    ...data?.nfts?.map((nft) => {
+                      return [
+                        <Avatar isRounded theme="image" />,
+                        getEllipsisTxt(nft.associatedTokenAddress),
+                        <Tag color="yellow" text="SPL NFT" />,
+                        1,
+                      ];
+                    }),
+                  ]}
+                  header={[
+                    "",
+                    <span>Name</span>,
+                    <span>Type</span>,
+                    <span>Amount</span>,
+                  ]}
+                  maxPages={3}
+                  noPagination
+                  onPageNumberChanged={function noRefCheck() {}}
+                  pageSize={5}
+                />
+              )}
+            </>
+          ) : (
+            <>
+              <h1>Solana DeFi Dashboard</h1>
+              <Button
+                onClick={() => authenticate({ type: "sol" })}
+                isLoading={isAuthenticating}
+                loadingText="Authenticating..."
+                text="Connect Wallet"
+              />
+            </>
+          )}
+        </>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { MoralisProvider } from "react-moralis";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider
+      initializeOnMount
+      appId={process.env.REACT_APP_MORALIS_APP_ID}
+      serverUrl={process.env.REACT_APP_MORALIS_SERVER_URL}
+    >
+      <App />
+    </MoralisProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
